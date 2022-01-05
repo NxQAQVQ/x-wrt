a52e34168dec194cd344f24b2256101855ebd7d1
Chen Minqiang <ptpt52@gmail.com>
2022-01-06 05:58:25 +0800

    ipq40xx: silence debug info

--- target/linux/ipq40xx/files/drivers/net/ethernet/qualcomm/essedma/edma.c
+++ target/linux/ipq40xx/files/drivers/net/ethernet/qualcomm/essedma/edma.c
@@ -1495,7 +1495,6 @@ int edma_rx_flow_steer(struct net_device *dev, const struct sk_buff *skb,
 	int res;
 
 	if (skb->protocol == htons(ETH_P_IPV6)) {
-		dev_err(&adapter->pdev->dev, "IPv6 not supported\n");
 		res = -EINVAL;
 		goto no_protocol_err;
 	}
